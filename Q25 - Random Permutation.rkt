6aefacba7593ae35521d9e9c44f941104d415875
Deepak Parpyani <dparpyani@yahoo.com>
2012-12-26 22:51:18 -0500

    Q25 added; also added .gitignore

---
+++ Q25 - Random Permutation.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+;; Question:
+;; Generate a random permutation of the elements of a list.
+;; Example:
+;; (rnd-permu '(a b c d e f))
+;;  => '(b a d c e f)
+
+(require (file "Q23 - Random-Select.rkt"))
+
+(define (rnd-permu lst)
+  (rnd-select lst (length lst)))
