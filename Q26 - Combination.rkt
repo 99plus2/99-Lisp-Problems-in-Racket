eb12644786005c1386e736d024f04322401ad021
Deepak Parpyani <dparpyani@yahoo.com>
2012-12-29 01:24:00 -0500

    Added Q26 & Q28

---
+++ Q26 - Combination.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+;; Question:
+;; Generate the combinations of K distinct objects chosen from the N elements of a list
+;; In how many ways can a committee of 3 be chosen from a group of 12 people? We all know that
+;; there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficients).
+;; For pure mathematicians, this result may be great. But we want to really generate all the
+;; possibilities in a list.
+;;
+;; Example:
+;; (combination 3 '(a b c d e f))
+;;  => '((a b c) (a b d) (a b e) ...etc. )
+
+(define (combination k lst)
+  (define (combination-helper k lst)
+    (cond
+      [(or (empty? lst) (> k (length lst))) (list lst)]
+      [else (append (map (λ (x) (cons (first lst) x)) (combination-helper (sub1 k) (rest lst))) (combination-helper k (rest lst)))]))
+  (filter (λ (x) (= (length x) k)) (combination-helper k lst)))
---
+++ Q28 - Sorting Sublists.rkt
@@ -0,0 +1,46 @@
+#lang racket
+
+;; Question:
+;; Sorting a list of lists according to length of sublists:
+;;
+;; a) We suppose that a list contains elements that are lists themselves. The
+;;    objective is to sort the elements of this list according to their length.
+;;    i.e. short lists first, longer lists later, or vice versa.
+;;
+;;    Example:
+;;    (lsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+;;     => '((o) (d e) (d e) (m n) (a b c) (f g h) (i j k l))
+;;
+;; b) Again, we suppose that a list contains elements that are lists themselves. But
+;;    this time the objective is to sort the elements of this list according to their
+;;    length frequency. i.e. in the default, where sorting is done ascendingly, lists
+;;    with rare lengths are placed first, others with a more frequent length come later.
+;;
+;;    Example:
+;;    (lfsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+;;     => '((i j k l) (o) (a b c) (f g h) (d e) (d e) (m n))
+;;
+;;    Note that in the above example, the first two lists in the result have length 4 and 1,
+;;    both lengths appear just once. The third and forth list have length 3 which appears twice
+;;    (there are two list of this length). And finally, the last three lists have length 2.
+;;    This is the most frequent length.
+;;
+
+(define (lsort lst)
+  (sort lst < #:key length #:cache-keys? #t))
+
+(define (lfsort lst)
+  (define (lfrequency item lst)
+    (define item-length (length item))
+    (foldl (λ (x y) (if (= (length x) item-length) (add1 y) y)) 0 lst))
+  
+  (define (lfsort-helper lst)
+    (cond
+      [(empty? lst) empty]
+      [else (define first-freq (lfrequency (first lst) lst))
+            (append (lfsort (filter (λ (x) (< (lfrequency x lst) first-freq)) (rest lst)))
+                    (list (first lst))
+                    (lfsort (filter (λ (x) (>= (lfrequency x lst) first-freq)) (rest lst))))]))
+  
+  (lfsort-helper lst))
+  
