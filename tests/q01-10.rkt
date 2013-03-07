a99a984e6d5472e64ac4a3aa2d84b4707744a8db
Deepak Parpyani <dparpyani@yahoo.com>
2013-03-06 23:39:31 -0500

    Added tests upto q10.

--- tests/q01-10.rkt
+++ tests/q01-10.rkt
@@ -27,12 +27,48 @@
 (assert (equal? (my-but-last `((a b) c (d e))) `(c (d e))))
 
 ;; Q03 tests
+(assert (equal? (element-at '(a b) 1) 'a))
 (assert (equal? (element-at '(a b c d e) 3) 'c))
+(assert (equal? (element-at '(a b c d e f) 6) 'f))
 
 ;; Q04 tests
+(assert (equal? (my-length '()) 0))
+(assert (equal? (my-length '(a)) 1))
+(assert (equal? (my-length '(a b c d e)) 5))
+
 ;; Q05 tests
+(assert (equal? (my-reverse '()) '()))
+(assert (equal? (my-reverse '(a)) '(a)))
+(assert (equal? (my-reverse '((0) (1))) '((1) (0))))
+(assert (equal? (my-reverse '(a b c d e)) '(e d c b a)))
+
 ;; Q06 tests
+(assert (palindrome? '()))
+(assert (palindrome? '(a)))
+(assert (palindrome? '(a o a)))
+(assert (palindrome? '(x a m a x)))
+(assert (not (palindrome? '(a b))))
+
 ;; Q07 tests
+(assert (equal? (my-flatten '()) '()))
+(assert (equal? (my-flatten '((a) ())) '(a)))
+(assert (equal? (my-flatten '(() ((()) ((())) ()))) '()))
+(assert (equal? (my-flatten '(a (b (c d) e))) '(a b c d e)))
+
 ;; Q08 tests
+(assert (equal? (compress '()) '()))
+(assert (equal? (compress '(() () ())) '(())))
+(assert (equal? (compress '(a 1 1 1 a 1 1)) '(a 1 a 1)))
+(assert (equal? (compress '(a a a a b c c a a d e e e e)) '(a b c a d e)))
+
 ;; Q09 tests
+(assert (equal? (pack '()) '()))
+(assert (equal? (pack '(() () ())) '((() () ()))))
+(assert (equal? (pack '(a 1 1 1 a 1 1)) '((a) (1 1 1) (a) (1 1))))
+(assert (equal? (pack '(a a a a b c c a a d e e e e)) '((a a a a) (b) (c c) (a a) (d) (e e e e))))
+
 ;; Q10 tests
+(assert (equal? (encode '()) '()))
+(assert (equal? (encode '(() () ())) '((3 ()))))
+(assert (equal? (encode '(a 1 1 1 a 1 1)) '((1 a) (3 1) (1 a) (2 1))))
+(assert (equal? (encode '(a a a a b c c a a d e e e e)) '((4 a) (1 b) (2 c) (2 a) (1 d) (4 e))))
