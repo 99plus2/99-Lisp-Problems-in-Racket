74f542a438a3b7ad2ed7a7bf2ec779348eadb787
Deepak Parpyani <dparpyani@yahoo.com>
2013-03-08 00:11:49 -0500

    Added debug option to test script.

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,15 @@
 # Runs tests for this project.
 # [needs Racket in $PATH]
 
-racket tests.rkt
+# File containing tests
+testfile="tests.rkt"
+
+if [ "${1}" = "-d" ]; then
+    # Debug Option
+    racket "${testfile}";
+else
+    # Redirect error to screen and output to '/dev/null'
+    racket "${testfile}" 2>&1 1>/dev/null
+fi
+
 
